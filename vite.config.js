6a004e29d5c2610f65bd6482f95724c7ffec3e2a
Shyamsundhar <shyam.sam0704@gmail.com>
2026-04-16 21:58:15 +0530

    Fix package.json and deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/Portfolio/"
 });
-=======
-  base: "/shyamsundhar-portfolio/"
-});
->>>>>>> 581c05029380d57324ee956efe7f469ed22d1e7e
